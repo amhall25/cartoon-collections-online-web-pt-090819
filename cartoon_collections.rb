2a84f51f1d4f9c0ec225b0eab04ba3d857a2402f
alycia hall <amhall25@gmail.com>
2019-09-21 22:45:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,9 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.with.index(1) do |dwarf, index|
+    puts "#{index}. #{dwarf}"
+  end
+  
+  
 end
 
 def summon_captain_planet# code an argument here
